524e182b2075fdc5a43703e079abc150995a070b
Nicolas-Vial <navial@miuandes.cl>
2021-05-02 18:17:09 -0400

    FINAL

--- fibonacci.js
+++ fibonacci.js
@@ -5,7 +5,22 @@ const readline = require('readline').createInterface({
    
   readline.question('Please type a number: ', ask => {
     var number=parseInt(ask);
-    console.log(number)
+    
+    if (number==0){
+        console.log("\nThe fibonacci value is: 0");
+    } else if (number==1){
+        console.log("\nThe fibonacci value is: 1");
+    }else{
+        var n_2=0;
+        var n_1=1;
+        var result;
+        for(var counter=1; counter<number;counter++){
+            result=n_1+n_2;
+            n_2=n_1;
+            n_1=result;
+        }
+        console.log("\nThe fibonacci value is: "+result);
+    }
     
     readline.close();
   });
